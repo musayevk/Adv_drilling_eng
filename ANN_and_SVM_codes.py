47a4588c3a86b8b270552ce2183a5eb11fddd893
musayevk <98299708+musayevk@users.noreply.github.com>
2023-04-30 10:04:54 -0500

    Update ANN_and_SVM_codes.py

--- ANN_and_SVM_codes.py
+++ ANN_and_SVM_codes.py
@@ -11,6 +11,11 @@ from sklearn.model_selection import GridSearchCV
 
 
 def build_model(hiddenLayerSizes, train_data):
+    """
+    This function builds an ANN sequential model given the hidden layer sizes and training data.
+    
+    Returns the compiled model.
+    """
 
     model =tf.keras.Sequential()
     model.add(tf.keras.layers.Dense(hiddenLayerSizes[0],input_dim=train_data.shape[1],activation='relu'))
@@ -31,18 +36,31 @@ def build_model(hiddenLayerSizes, train_data):
 
 
 def optimal_ANN(verbose=True):
+    """
+    This function computes the optimal ANN architecture. Architectures are defined inside.
+    
+    Returns the best model with relevant informations.
+    
+    """
+    #initialize
     best_score=0
-    architecture=[32, 16], [32, 32], [16, 8], [64, 32, 16]
     best_network=None
-    for epoch in range(5, 200):
+    
+    #the hidden layer neurons considered
+    architecture=[32, 32], [32, 16, 8], [32, 32, 32], [16, 8, 4], [64, 32, 16]
+    
+    for epoch in range(5, 500):
         for params in architecture:
+            #create a model
             model = build_model(params, train_x)
+            #train the model
             history= model.fit(train_x, train_y, epochs=epoch,
                       batch_size=32,validation_split=0.0, verbose=0)
-            # pd.DataFrame(history.history).plot()
-            # plt.show()
+            #predict the testing data
             trial_ANN_predicted=model.predict(test_x,verbose=0)
+            #compute the R2 value
             score=sklearn.metrics.r2_score(test_y, trial_ANN_predicted)
+            #save the best model
             if score>best_score:
                 best_epoch=epoch
                 best_model=model
@@ -51,15 +69,16 @@ def optimal_ANN(verbose=True):
                 best_network=params
         if verbose:
             print('epoch:',epoch,'best:',best_score, 'best_network:',best_network)
-
+    
+    #plot the best model crossplot
     plt.plot(ANN_predicted, test_y, "bo")
     plt.plot(range(2),range(2),'r')
-    plt.xlim((0,0.3))
-    plt.ylim((0,0.3))
+    plt.xlim((0,0.4))
+    plt.ylim((0,0.4))
     
     return best_network, best_epoch, best_score, best_model
 
-
+#run the optimization function 
 best_network, best_epoch, best_score, best_model=optimal_ANN(verbose=1)
 
 # saved the model with the best R2
@@ -67,19 +86,32 @@ best_model.save('BestANN_raw_data_80_20_split_yu')
 
 
 def optimal_svm(verbose=True):
-
+    """
+    This function computes the optimal SVM hyperparameters.
+    
+    Returns the best model.
+    
+    """
+    #define the ranges for the hyperparameters
     c_range=[0.1, 1, 10, 100, 500]
     gamma_range=[1, 0.1, 0.01, 0.001, 0.0001]
     eps_range=[0.1, 0.1, 0.01, 0.001, 0.0001]
+    
+    #initialize
     best_score=0
     best_model, best_params=None, None
     for c in c_range:
         for g in gamma_range:
             for e in eps_range:
+                #compile
                 svm_reg = svm.SVR(C=c, kernel='rbf',epsilon=e,gamma=g)
+                #train the model
                 svm_reg.fit(train_x, np.asarray(train_y).flatten())
+                #predict for testing data
                 svm_predict=svm_reg.predict(test_x)
+                #compute the R2 value
                 score=sklearn.metrics.r2_score(test_y, list(svm_predict))
+                #save the best model
                 if score>best_score:
                     best_model=svm_reg
                     best_score=score
@@ -91,6 +123,7 @@ def optimal_svm(verbose=True):
     plt.plot(range(2),range(2),'r')
     plt.xlim((0,0.3))
     plt.ylim((0,0.3))
+    
     return best_model
 
 svm_optimal_model=optimal_svm()
